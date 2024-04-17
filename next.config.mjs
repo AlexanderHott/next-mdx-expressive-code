26eb0865b5092412b43c7a0269b62585f212fcc2
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-04-17 13:08:39 -0400

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx'
+import rehypeExpressiveCode from 'rehype-expressive-code'
 
-export default nextConfig;
+/** @type {import('rehype-expressive-code').RehypeExpressiveCodeOptions} */
+const rehypeExpressiveCodeOptions = {
+  // You can add configuration options here
+}
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+}
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [
+      // The nested array structure is required to pass options
+      // to a rehype plugin
+      [rehypeExpressiveCode, rehypeExpressiveCodeOptions],
+    ],
+  },
+})
+
+export default withMDX(nextConfig)
